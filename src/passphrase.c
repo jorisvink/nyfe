9c2534f22dc47a3b3c5923a2b13781ae744f7712
Joris Vink <joris@sanctorum.se>
2025-05-31 21:34:36 +0200

    Use correct defines

--- src/passphrase.c
+++ src/passphrase.c
@@ -17,7 +17,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#if !defined(__APPLE__) && !defined(__WIN64__)
+#if !defined(__APPLE__) && !defined(NYFE_PLATFORM_WINDOWS)
 #include <endian.h>
 #endif
 
